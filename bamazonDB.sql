87cfba2d4d3dafb42eea35c08c3f3271aaf883d6
Brian Padilla <bpadil123@gmail.com>
2018-08-09 18:40:18 -0600

    first push

---
+++ bamazonDB.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+ item_id INT(30) NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(10) NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT(30),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, deparment_name, price, stock_quantity)
+VALUES ("Bike", "Sporting Goods", 100, 15),
+("Fishing Rod", "Sporting Goods", 35, 20);
