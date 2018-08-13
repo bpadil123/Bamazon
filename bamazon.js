7e39ef2b5233f48c88172b7db676549f7b62ba06
Brian Padilla <bpadil123@gmail.com>
2018-08-12 22:36:36 -0600

    connect to SQL, prompt showing up incorrectly

--- bamazon.js
+++ bamazon.js
@@ -1,12 +1,11 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
   // Your password
@@ -14,16 +13,62 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
+
+
+  showProducts();
 });
 
-function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
+function showProducts() {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    console.log(res);
-    connection.end();
-  });
+    // console.log(res);
+    connection.end;
+
+    console.log(' ~~~~~~~~~~~~~ Welcome to BAMazon ~~~~~~~~~~~~~  ');
+    console.log('----------------------------------------------------------------------------------------------------');
+    for (var i = 0; i < res.length; i++) {
+      console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+      console.log('--------------------------------------------------------------------------------------------------')
+    }
+    console.log(" ");
+  })
 }
+
+inquirer.prompt([{
+    type: "input",
+    name: "selection",
+    message:"What would you like to purchase? Please input the ID that is to the left of the item you would like?    ",
+  },
+  {
+    type: "input",
+    name: "quantity",
+    message:"How many would you like to purchase?",
+  }
+]).then(function (inquiry) {
+  var idSelect = inquiry.selection
+  connection.query('SELECT * FROM products WHERE id = ' + idSelect, function (err, res) {
+    let currentStock = res[0].stock_quantity;
+    var quantSelect = inquiry.quantity
+
+    if (err) console.log(err);
+
+    if (quantSelect > currentStock) {
+      console.log("We appologize, we do not have enough stock in our inventory, please see the inventory qty and try again.")
+
+
+    } else {
+      currentStock -= quantSelect
+
+      connection.query('UPDATE products SET stock_quantity =' + currentStock + ' WHERE id =' + idSelect, function (err) {
+        if (err) throw err;
+
+        console.log("Great Choice! We will see you soon!")
+      })
+    }
+  })
+
+
+})
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -1,30 +1,16 @@
-
-DROP DATABASE IF EXISTS bamazonDB;
-
-CREATE DATABASE bamazonDB;
+¡CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
 
 CREATE TABLE products (
- id INT(30) NOT NULL AUTO_INCREMENT,
+ item_id INT(30) NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(45) NULL,
-  department_name VARCHAR(40) NULL,
+  department_name VARCHAR(10) NULL,
   price DECIMAL(10,2) NULL,
   stock_quantity INT(30),
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, deparment_name, price, stock_quantity)
 VALUES ("Bike", "Sporting Goods", 100, 15),
-("Fishing Rod", "Sporting Goods", 35, 20),
-("Playstation", "Video Games", 300, 15),
-("Nintendo Switch", "Video Games", 350, 30),
-("Sofa", "Furniture", 600, 5),
-("Bed Frame", "Furniture", 700, 4),
-("Toaster", "Appliances", 30, 25),
-("SodaStream", "Appliances", 100, 12),
-("Legos", "Toys", 60, 40),
-("Drone", "Toys", 800, 5);
-
-
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Leo2691b';
+("Fishing Rod", "Sporting Goods", 35, 20);
